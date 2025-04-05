848d9847d55f226544aacfc27499a92143b4172a
KinzaaSheikh <kinzasheikhsiddiqui@gmail.com>
2025-04-05 17:44:09 +0500

    Update README

---
+++ README.md
@@ -0,0 +1,5 @@
+## Ensure dependencies are installed
+uv sync
+
+## Run the chatbot
+uv run chainlit run src/chatbot.py -w
--- src/chatbot.py
+++ src/chatbot.py
@@ -1,8 +1,7 @@
 import chainlit as cl
 
-@cl.on_message()
-
+@cl.on_message
 async def main(message: cl.Message):
     await cl.Message(
         content=f"Received: {message.content}"
-    )
+    ).send()
