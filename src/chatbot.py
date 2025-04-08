87fed1371338890f403021c2005db0db35db1cd1
KinzaaSheikh <kinzasheikhsiddiqui@gmail.com>
2025-04-08 21:37:20 +0500

    Streaming output

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-from typing import cast
 import chainlit as cl
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from openai.types.responses import ResponseTextDeltaEvent
 from agents.run import RunConfig
 
 # Load environment variables from .env file
@@ -37,86 +37,42 @@ async def start():
     )
 
     # Step 4: Set agent
-    agent1: Agent = Agent(
+    book_agent: Agent = Agent(
         name="Book Recommender Agent",
         instructions="You are a helpful agent that suggests books to read based on the user's prompt.",
         model=model
         )
     
-    @cl.on_chat_start
-    async def handle_chat_start():
-        cl.user_session.set("history", [])
-        await cl.Message(
-            content="Welcome to the Hyper-specific book recommendation chatbot! How can I assist you today?"
-            ).send()
-    
-    @cl.on_message
-    async def handle_message(message: cl.Message):
+    cl.user_session.set("book_agent", book_agent)
+    cl.user_session.set("config", config)
+    cl.user_session.set("history", [])
 
-        history = cl.user_session.get("history") or []
-        # Standard Interface [{role: user, content: message.content}]
-        history.append({"role": "user", "content": message.content})
-        result = await Runner.run(
-            agent1,
-            input=history,
-            run_config=config,
-        )
+    await cl.Message(
+        content="Welcome to the Hyper-specific book recommendation chatbot! How can I assist you today?"
+        ).send()
 
-        history.append({"role": "assistant", "content": result.final_output})
-        cl.user_session.set("history", history)
 
-        await cl.Message(content=result.final_output).send()
+@cl.on_message
+async def handle_message(message: cl.Message):
+    book_agent: Agent = cl.user_session.get("book_agent")
+    config: RunConfig = cl.user_session.get("config")
+    history = cl.user_session.get("history")
 
-    """"Set up the chat session when a user connects"""
-    # # Initial an empty chat history in the session
-    # cl.user_session.set("chat_history", [])
+    msg = cl.Message(content="")
+    await msg.send()
+       
+   # Standard Interface [{role: user, content: message.content}]
+    history.append({"role": "user", "content": message.content})
 
-    # # Set the initial prompt for the chat session
-    # cl.user_session.set("config", config)
-    # cl.user_session.set("agent", agent)
+    result = Runner.run_streamed(
+        book_agent,
+        input=history,
+        run_config=config,
+    )
 
-    # # Set the initial prompt for the chat session
-    # await cl.Message(
-    #     content="Welcome to the Hyper-specific book recommendation chatbot! How can I assist you today?"
-    #     ).send()
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            await msg.stream_token(event.data.delta)
 
-    # @cl.on_message
-    # async def main(message: cl.Message):
-    #     """Process incoming messages and generate responses."""
-    #     # Send a thinking message
-    #     msg = cl.Message(content="Thinking...")
-    #     await msg.send()
-
-    #     agent: Agent = cast(Agent, cl.user_session.get("agent"))
-    #     config: RunConfig = cast(RunConfig, cl.user_session.get("config"))
-
-    #     # Retrieve the chat history from the session.
-    #     history = cl.user_session.get("chat_history") or []
-        
-    #     # Append the user's message to the history.
-    #     history.append({"role": "user", "content": message.content})
-        
-
-    #     try:
-    #         print("\n[CALLING_AGENT_WITH_CONTEXT]\n", history, "\n")
-    #         result = Runner.run_sync(starting_agent = agent,
-    #                     input=history,
-    #                     run_config=config)
-            
-    #         response_content = result.final_output
-            
-    #         # Update the thinking message with the actual response
-    #         msg.content = response_content
-    #         await msg.update()
-        
-    #         # Update the session with the new history.
-    #         cl.user_session.set("chat_history", result.to_input_list())
-            
-    #         # Optional: Log the interaction
-    #         print(f"User: {message.content}")
-    #         print(f"Assistant: {response_content}")
-            
-    #     except Exception as e:
-    #         msg.content = f"Error: {str(e)}"
-    #         await msg.update()
-    #         print(f"Error: {str(e)}")
+    history.append({"role": "assistant", "content": result.final_output})
+    cl.user_session.set("history", history)
